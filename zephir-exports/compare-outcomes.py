fb39f89560abed75356476f8e83dc59e0247a13e
cscollett <charlie.collett@ucop.edu>
2018-11-05 11:21:19 -0800

    Add files to compare winner output

---
+++ zephir-exports/compare-outcomes.py
@@ -0,0 +1,10 @@
+import json
+with open('outcome1.tsv') as f:
+    with open('outcome2.tsv') as g:
+        for idx, f_line in enumerate(f):
+            first = f_line
+            second = g.readline()
+            if first != second:
+                print(idx+1)
+                print(first)
+                print(second)
---
+++ zephir-exports/create_winner_list.py
@@ -0,0 +1,20 @@
+import json
+
+import pymarc
+from sqlalchemy import create_engine
+import zlib
+
+with open("outcome1.tsv", "a") as outcome_file:
+
+    engine = create_engine(
+        "sqlite:///cache/complete-gz.db",
+        echo=False,
+    )
+    with engine.connect() as con:
+        create_table_stmt = "select cache_data from cache"
+        result = con.execute(create_table_stmt)
+        for idx, row in enumerate(result):
+            for marc_record in pymarc.JSONReader(zlib.decompress(row[0]).decode('utf8')):
+                cid = marc_record['CID']["a"]
+                htid = marc_record['HOL']["p"]
+                outcome_file.write(cid+"\t"+htid+"\n")
