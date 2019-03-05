b06cec8515101d444a7868b548eb1318ca19f3ca
cscollett <charlie.collett@ucop.edu>
2019-03-05 15:14:01 -0800

    Revert "Test travis will fail with black fail"
    This reverts commit 236a7e957898b4541d60ee874623d55177351443.

--- zephir-cluster/tests/test_find_cluster.py
+++ zephir-cluster/tests/test_find_cluster.py
@@ -11,7 +11,11 @@ def env_setup(td_tmpdir, monkeypatch):
     monkeypatch.setenv(
         "ZED_OVERRIDE_CONFIG_PATH", os.path.join(str(td_tmpdir), "config")
     )
-    os.system("mysql --host=localhost --user=root  < {}/cid_related_tables.sql".format(td_tmpdir))
+    os.system(
+        "mysql --host=localhost --user=root  < {}/cid_related_tables.sql".format(
+            td_tmpdir
+        )
+    )
 
 
 def test_find_cluster_with_no_files(env_setup, capsys):
