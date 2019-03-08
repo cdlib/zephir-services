a2fe366279457dbe73109bebae7fcce59d93997a
cscollett <charlie.collett@ucop.edu>
2019-03-07 16:29:37 -0800

    Cleanup from Flake8 and Black

--- utility/start-mysql-test-server.py
+++ utility/start-mysql-test-server.py
@@ -3,16 +3,30 @@ import subprocess
 
 try:
     # create a tmp directory for all of the mysql data
-    mktemp_output = subprocess.check_output("MYSQL_DATA=$(mktemp -d /tmp/MYSQL-XXXXX); echo $MYSQL_DATA", shell=True)
+    mktemp_output = subprocess.check_output(
+        "MYSQL_DATA=$(mktemp -d /tmp/MYSQL-XXXXX); echo $MYSQL_DATA", shell=True
+    )
     MYSQL_DATA = mktemp_output.decode("utf-8").strip()
     # create a tmp log file
-    subprocess.check_output("touch {0}/out.log".format(MYSQL_DATA), shell=True);
+    subprocess.check_output("touch {0}/out.log".format(MYSQL_DATA), shell=True)
     # start the mysql server
-    mysqld_output = subprocess.check_output("mysqld --datadir={0} --pid-file={0}/mysql.pid --socket={0}/mysql.socket --skip-networking --skip-grant-tables &> {0}/out.log &".format(MYSQL_DATA), shell=True, env=dict(os.environ))
+    mysqld_output = subprocess.check_output(
+        "mysqld --datadir={0} --pid-file={0}/mysql.pid --socket={0}/mysql.socket --skip-networking --skip-grant-tables &> {0}/out.log &".format(
+            MYSQL_DATA
+        ),
+        shell=True,
+        env=dict(os.environ),
+    )
     # set compatability for python sqlalchemy package
-    compatability_output = subprocess.check_output("mysql -e 'set @@global.show_compatibility_56=ON", shell=True)
+    compatability_output = subprocess.check_output(
+        "mysql -e 'set @@global.show_compatibility_56=ON", shell=True
+    )
     # export the socket (will be used by mysql to connect) and pid file (will be used for shutdown)
-    print("export MYSQL_UNIX_PORT={0}/mysql.socket; export MYSQL_PID_FILE={0}/mysql.pid;\n".format(MYSQL_DATA))
+    print(
+        "export MYSQL_UNIX_PORT={0}/mysql.socket; export MYSQL_PID_FILE={0}/mysql.pid;\n".format(
+            MYSQL_DATA
+        )
+    )
     print("echo 'Mysql test server started'")
 except subprocess.CalledProcessError as grepexc:
-    print("echo '", grepexc.returncode, grepexc.output,"'")
+    print("echo '", grepexc.returncode, grepexc.output, "'")
--- utility/stop-mysql-test-server.py
+++ utility/stop-mysql-test-server.py
@@ -2,9 +2,11 @@ import os
 import subprocess
 
 try:
-    MYSQL_PID_FILE = os.environ['MYSQL_PID_FILE']
-    mktemp_output = subprocess.check_output("kill -9 `cat {}`".format(MYSQL_PID_FILE), shell=True)
+    MYSQL_PID_FILE = os.environ["MYSQL_PID_FILE"]
+    mktemp_output = subprocess.check_output(
+        "kill -9 `cat {}`".format(MYSQL_PID_FILE), shell=True
+    )
     print("unset MYSQL_UNIX_PORT; unset MYSQL_PID_FILE;\n")
     print("echo 'Mysql test server stopped'")
 except subprocess.CalledProcessError as grepexc:
-    print("echo '", grepexc.returncode, grepexc.output,"'")
+    print("echo '", grepexc.returncode, grepexc.output, "'")
