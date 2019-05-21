f70c9f5ea53f961a9702c8aae40eaa82541569c9
cscollett <charlie.collett@ucop.edu>
2019-05-21 11:10:33 -0700

    Remove unused files

--- zephir-exports/compare-outcomes.py
+++
@@ -1,9 +0,0 @@
-with open("outcome1.tsv") as f:
-    with open("outcome2.tsv") as g:
-        for idx, f_line in enumerate(f):
-            first = f_line
-            second = g.readline()
-            if first != second:
-                print(idx + 1)
-                print(first)
-                print(second)
