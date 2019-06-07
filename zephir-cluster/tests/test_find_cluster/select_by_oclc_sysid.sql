4b632a39ecf536f60bdc1e0815a46ad7a86a8133
cscollett <charlie.collett@ucop.edu>
2019-06-07 11:05:33 -0700

    Disregard file approach to prototype

--- zephir-cluster/tests/test_find_cluster/select_by_oclc_sysid.sql
+++
@@ -1,7 +0,0 @@
-select * from zephir_identifier_records zir
-join zephir_identifiers zi on zir.identifier_autoid = zi.autoid
-join zephir_records zr on zr.autoid = zir.record_autoid
-where (type='oclc' and identifier in ('10115627', '10123266'))
-or (type = 'contrib_sys_id' and identifier= 'nrlfGLAD307998-B')
-order by zi.type desc, cid;
-
