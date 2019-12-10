45f53770318f1026edcc556d4e0e087c7628f720
Charlie Collett <charlie.collett@ucop.edu>
2019-12-10 11:42:08 -0800

    Run black

--- zed-logger/tests/test_zed_event.py
+++ zed-logger/tests/test_zed_event.py
@@ -6,15 +6,15 @@ import pytest
 from zed_event import ZedEvent
 
 
-
 def test_create_zed_event(td_tmpdir):
-    with open(os.path.join(td_tmpdir,"complete_event.json"), "r") as f:
+    with open(os.path.join(td_tmpdir, "complete_event.json"), "r") as f:
         event_json = json.load(f)
     e = ZedEvent(event_json)
     assert json.dumps(event_json) == str(e)
 
+
 def test_create_zed_event(td_tmpdir):
-    with open(os.path.join(td_tmpdir,"missing_timestamp.json"), "r") as f:
+    with open(os.path.join(td_tmpdir, "missing_timestamp.json"), "r") as f:
         event_json = json.load(f)
     e = ZedEvent(event_json)
     assert json.dumps(event_json) == str(e)
