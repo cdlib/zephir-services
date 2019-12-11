d2d1c35432f533c874a8b8156a366e4a1ba836a0
Charlie Collett <charlie.collett@ucop.edu>
2019-12-11 13:04:57 -0800

    Refactor and add coverage

---
+++ zed-event/tests/test_zed_event.py
@@ -0,0 +1,41 @@
+import json
+import os
+
+import jsonschema
+import pytest
+
+from zed_event import ZedEvent
+
+
+def test_create_zed_event(td_tmpdir):
+    with open(os.path.join(td_tmpdir, "complete_event.json"), "r") as f:
+        event_dict = json.load(f)
+    e = ZedEvent(event_dict)
+    assert e.isvalidated() is True
+    assert json.dumps(event_dict) == str(e)
+
+def test_init_fields_default(td_tmpdir):
+    with open(os.path.join(td_tmpdir, "missing_init_fields.json"), "r") as f:
+        event_dict = json.load(f)
+    e = ZedEvent(event_dict, validate=False)
+    assert e.event().get("timestamp", None) is not None
+    assert e.event().get("process", None) is not None
+    assert e.event().get("event", None) is not None
+
+def test_do_not_init_fields(td_tmpdir):
+    with open(os.path.join(td_tmpdir, "missing_init_fields.json"), "r") as f:
+        event_dict = json.load(f)
+    e = ZedEvent(event_dict, init=None, validate=False)
+    assert e.event().get("timestamp", None) is None
+    assert e.event().get("process", None) is None
+    assert e.event().get("event", None) is None
+
+def test_skip_validation_errors():
+    event_dict = {}
+    e = ZedEvent(event_dict, init=None, validate=False)
+    assert e.isvalidated() is False
+
+def test_validation_errors_default(td_tmpdir):
+    event_dict = {}
+    with pytest.raises(jsonschema.exceptions.ValidationError):
+        e = ZedEvent(event_dict)
--- zed-event/zed_event.py
+++ zed-event/zed_event.py
@@ -10,7 +10,8 @@ import jsonschema
 class ZedEvent:
     """ Zed Event """
 
-    # Load ZED schemas for validation
+    # CLASS BLOCK
+    # Define schemas and validation methods for Zed Events.
     __schemas_dir = os.path.join(os.path.dirname(__file__), "schemas")
     __schemas = {}
     for filename in os.listdir(os.path.join(os.path.dirname(__file__), "schemas")):
@@ -20,6 +21,27 @@ class ZedEvent:
                 schema = json.loads(schema_data)
                 __schemas[re.search("^(.*)_schema", filename).group(1).upper()] = schema
 
+    @classmethod
+    def schemas(self, schema):
+        return ZedEvent.__schemas[schema]
+
+    @classmethod
+    def validate(self, event, schema="ZED"):
+        jsonschema.validate(event, ZedEvent.schemas(schema))
+        return True
+
+    @classmethod
+    def generate_key(self):
+        return str(uuid.uuid4())
+
+    @classmethod
+    def generate_timestamp(self):
+        return datetime.now(timezone.utc).strftime(
+            "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
+
+    # INSTANCE BLOCK
+    # Define ZedEvent instances for event logging
     def __init__(
         self,
         event={},
@@ -28,31 +50,29 @@ class ZedEvent:
         schema="ZED",
     ):
         self._event = event
-        self._is_validated = False
+        self._validated = False
 
         if init is None:
             init = []
         if "event" in init and ("event" not in self._event or not self._event["event"]):
-            self._event["event"] = str(uuid.uuid4())
+            self._event["event"] = ZedEvent.generate_key()
         if "process" in init and (
             "process" not in self._event or not self._event["process"]
         ):
-            self._event["process"] = str(uuid.uuid4())
+            self._event["process"] = ZedEvent.generate_key()
         if "timestamp" in init and (
             "timestamp" not in self._event or not self._event["timestamp"]
         ):
-            self._event["timestamp"] = datetime.now(timezone.utc).strftime(
-                "%Y-%m-%dT%H:%M:%S.%fZ"
-            )
+            self._event["timestamp"] = ZedEvent.generate_timestamp()
 
         if validate:
-            ZedEvent.validate(self._event, schema)
+            self._validated = ZedEvent.validate(self._event, schema)
+
+    def event(self):
+        return self._event
+
+    def isvalidated(self):
+        return self._validated
 
     def __str__(self):
-        print(json.dumps(self._event))
         return json.dumps(self._event)
-
-    @classmethod
-    def validate(self, event, schema="ZED"):
-        jsonschema.validate(event, ZedEvent.__schemas[schema])
-        self.validated = True
