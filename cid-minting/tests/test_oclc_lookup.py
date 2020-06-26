556eed7f17ac8d725dd4fd1f819a3e88df0a984f
Charlie Collett <charlie.collett@ucop.edu>
2020-06-26 15:50:47 -0700

    Rework json and database fixture use

--- cid-minting/tests/test_oclc_lookup.py
+++ cid-minting/tests/test_oclc_lookup.py
@@ -1,8 +1,15 @@
+import os
+
 import pytest
+import plyvel
 
 from oclc_lookup import get_primary_ocn
 
 
-def test_get_primary_ocn(split_cases, primary_ldb_path):
-    results = [get_primary_ocn(ocn, primary_ldb_path) for ocn in split_cases["input"]]
-    assert split_cases["expected"].sort() == results.sort()
+@pytest.mark.parametrize("prepare_primary_db_tests", ["primary.json"], indirect=True)
+def test_get_primary_ocn(td_tmpdir, prepare_primary_db_tests):
+    cases = prepare_primary_db_tests["cases"]
+    db_path = prepare_primary_db_tests["db_path"]
+    results = [get_primary_ocn(ocn, db_path) for ocn in cases["input"]]
+    assert cases["expected"].sort() == results.sort()
+    assert True
