708e4dbc32e92849ca32f7e064891c714fa9b1e2
Jing Jiang <jing.jiang@ucop.edu>
2020-07-10 15:02:46 -0700

    use config file for db connt

--- cid-minting/zephir_cluster_lookup.py
+++ cid-minting/zephir_cluster_lookup.py
@@ -1,8 +1,40 @@
+import os
+import sys
+import environs
 
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
-import mysql.connector as mysql 
+from lib.utils import ConsoleMessenger
+import lib.utils as utils
+
+SELECT_ZEPHIR = """select distinct z.cid, i.identifier
+    from zephir_records as z
+    inner join zephir_identifier_records as r on r.record_autoid = z.autoid
+    inner join zephir_identifiers as i on i.autoid = r.identifier_autoid
+    where i.type = 'oclc'
+"""
+AND_IDENTIFIER_IN = "and i.identifier in"
+ORDER_BY = "order by z.cid, z.id"
+
+def get_db_conn_string_from_config_by_key(config_dir_name, config_fname, key):
+    """return database connection string from db_config.yml file
+       config_dir: directory of configuration files
+       config_fname: configuration filename
+       key: configuration key
+    """
+    ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+    CONFIG_PATH = os.path.join(ROOT_PATH, config_dir_name)
+
+    # load all configuration files in directory
+    configs = utils.load_config(CONFIG_PATH)
+    print(configs)
+
+    # get config value by filename and key
+    config = configs.get(config_fname, {}).get(key)
+    print(config)
+
+    return str(utils.db_connect_url(config))
 
 class ZephirDatabase:
     def __init__(self, db_connect_str):
@@ -13,83 +45,33 @@ class ZephirDatabase:
             results = connection.execute(sql, params or ())
             return results.fetchall()
 
-def get_zephir_cluster_by_ocn(db_conn_str, sql):
+def get_zephir_cluster_by_ocn(db_conn_str, oclc_list):
+    sql = SELECT_ZEPHIR + " " + AND_IDENTIFIER_IN + " (" + oclc_list + ") " + ORDER_BY
     z = ZephirDatabase(db_conn_str)
     return z.query(text(sql))
 
+def valid_ocn_list(ocn_list):
+    pass
+
 def main():
+    if (len(sys.argv) > 1):
+        env = sys.argv[1]
+    else:
+        env = "test"
 
-    database = "htmm"
-    host = "rds-d2d-htmm-dev.cmcguhglinoa.us-west-2.rds.amazonaws.com"
-    user = "htmmrw"
-    password = "htmmrw3dev"
-    port = "3306"
+    # get environment variable in .env file
+    ENV = os.environ.get("MINTER_ENV") or env
+    print("env: {}".format(ENV))
 
-    sql_a = """select distinct z.cid, i.identifier
-    from zephir_records as z
-    inner join zephir_identifier_records as r on r.record_autoid = z.autoid
-    inner join zephir_identifiers as i on i.autoid = r.identifier_autoid
-    where i.type = 'oclc' and i.identifier in 
-    """
-    sql_b = "order by z.cid, z.id"
+    DB_CONNECT_STR = os.environ.get("OVERRIDE_DB_CONNECT_STR") or get_db_conn_string_from_config_by_key('config','minter_db', ENV)
+
+    print(DB_CONNECT_STR)
+
     oclc_list = "'6758168','15437990','5663662','33393343','28477569','8727632'"
-    sql = sql_a + " (" + oclc_list + ") " + sql_b
 
-# use sqlalchemy 
-    #db_conn_str = 'dialect+driver://user:pass@host:port/db'
-    #db_conn_str = "sqlite:///database/test_zephir_sqlite.db"
-    db_conn_str = "mysql+mysqlconnector://{}:{}@{}:{}/{}".format(user, password, host, port, database)
-
-    print("combined string results: ")
-    results = get_zephir_cluster_by_ocn(db_conn_str, sql)
+    results = get_zephir_cluster_by_ocn(DB_CONNECT_STR, oclc_list)
     print(results)
 
-
-# use mysql.connector
-    database_info = {'host': host, 'user': user, 'password': password, 'database': database}
-
-    print("mysql connector")
-    cluster = test_sql_connector(database_info, sql)
-    print(cluster)
-
-class ZephirDatabaseMysql:
-    def __init__(self, database):
-        self._conn = mysql.connect(**database)
-        self._cursor = self._conn.cursor()
-
-    @property
-    def connection(self):
-        return self._conn
-
-    @property
-    def cursor(self):
-        return self._cursor
-
-    def commit(self):
-        self.connection.commit()
-
-    def close(self, commit=True):
-        if commit:
-            self.commit()
-        self.connection.close()
-
-    def execute(self, sql, params=None):
-        self.cursor.execute(sql, params or ())
-
-    def fetchall(self):
-        return self.cursor.fetchall()
-
-    def fetchone(self):
-        return self.cursor.fetchone()
-
-    def query(self, sql, params=None):
-        self.cursor.execute(sql, params or ())
-        return self.fetchall()
-
-def test_sql_connector(database_info, sql):
-    zephir = ZephirDatabaseMysql(database_info)
-    zephir.execute(sql)
-    return zephir.fetchall()
 
 if __name__ == '__main__':
     main()
