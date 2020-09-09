ecf2ec61c9cc87d380633cbc5dbe371a3a61f518
Jing Jiang <jing.jiang@ucop.edu>
2020-09-09 16:39:22 -0700

    separate create table and insert data

---
+++ cid-minting/tests/test_cid_minting_store/cid_minter_prep_datasets.sql
@@ -0,0 +1,7 @@
+INSERT INTO cid_minting_store(type, identifier, cid)
+VALUES
+("oclc", "8727632", "002492721"),
+("contrib_sys_id", "pur215476", "002492721"),
+("oclc", "32882115", "011323405"),
+("contrib_sys_id", "pur864352", "011323405"),
+("contrib_sys_id", "uc1234567", "011323405");
