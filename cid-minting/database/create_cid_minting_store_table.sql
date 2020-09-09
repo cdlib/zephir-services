db1cb5423e67b62a15b4e2af142e3f8244c3c2b8
Jing Jiang <jing.jiang@ucop.edu>
2020-09-09 16:35:21 -0700

    added unique contrain and index to cid_minting_store_table

--- cid-minting/database/create_cid_minting_store_table.sql
+++ cid-minting/database/create_cid_minting_store_table.sql
@@ -1,10 +1,12 @@
-#DROP TABLE IF EXISTS cid_minting_store;
+DROP TABLE IF EXISTS cid_minting_store;
 
 CREATE TABLE IF NOT EXISTS cid_minting_store (
         id integer PRIMARY KEY AUTOINCREMENT,
         type char(50) DEFAULT NULL,
         identifier char(255) DEFAULT NULL,
         cid char(11) DEFAULT NULL,
-	updated DATETIME DEFAULT CURRENT_TIMESTAMP
-); 
+	updated DATETIME DEFAULT CURRENT_TIMESTAMP,
+        UNIQUE (type,identifier,cid)
+);  
 
+CREATE UNIQUE INDEX index_cid_minting_store_cid ON cid_minting_store(cid);
