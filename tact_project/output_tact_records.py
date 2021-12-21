fdb41c25f43104362f89dfadd91407a514818aa5
Jing Jiang <jing.jiang@ucop.edu>
2021-12-20 16:39:44 -0800

    insert multiple rows to a db table

--- tact_project/output_tact_records.py
+++ tact_project/output_tact_records.py
@@ -10,6 +10,7 @@ from sqlalchemy import text
 
 import lib.utils as utils
 from tact_db_utils import find_tact_transactions
+from tact_db_utils import insert_tact_transactions
 
 def test_read_tact_records():
 
@@ -20,10 +21,37 @@ def test_read_tact_records():
     configs= utils.get_configs_by_filename(CONFIG_PATH, CONFIG_FILE)
     db_connect_str = str(utils.db_connect_url(configs[env]))
 
+    print(db_connect_str)
+
     results = find_tact_transactions(db_connect_str, 1)
     print(results)
     for result in results:
         print("{},{}\n".format(result['id'], result['doi']))
+
+    table_name = "transactions"
+    trans_dict_list = [
+            {
+            'publisher': "ACM",
+            'doi': "111",
+            'article_title': "test title 1",
+            'uc_institution': "UCD",
+            },
+            {
+            'publisher': "ACM",
+            'doi': "222",
+            'article_title': "test title 2",
+            'uc_institution': "UCD",
+            }
+        ]
+
+    trans_dict = {
+            'publisher': "ACM",
+            'doi': "111",
+            'article_title': "test title 1",
+            'uc_institution': "UCD",
+            }
+
+    insert_tact_transactions(db_connect_str, table_name, trans_dict_list)
 
 if __name__ == '__main__':
     test_read_tact_records()
--- tact_project/tact_db_utils.py
+++ tact_project/tact_db_utils.py
@@ -3,18 +3,71 @@ import sys
 
 from sqlalchemy import create_engine
 from sqlalchemy import text
+from sqlalchemy import String
+from sqlalchemy import Integer
+from sqlalchemy import Column
+from sqlalchemy import column
+from sqlalchemy import table 
+from sqlalchemy import insert 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import SQLAlchemyError 
 
 SELECT_TACT = "SELECT id, publisher, doi FROM transactions WHERE id=:id"
+
+Base = declarative_base()
+
+class Transactions(Base):
+    __tablename__ = "transactions"
+    id = Column(Integer, primary_key=True)
+    publisher = Column(String(50))
+    doi = Column(String(50))
+    article_title = Column(String(150))
+    corresponding_author = Column(String(50))
+    corresponding_author_email = Column(String(50))
+    uc_institution = Column(String(50))
+    institution_identifier = Column(String(50))
+    document_type = Column(String(50))
+    eligible = Column(String(50))
+    inclusion_date = Column(String(50))
+    uc_approval_date = Column(String(50))
+    article_access_type = Column(String(50))
+    article_license = Column(String(50))
+    journal_name = Column(String(50))
+    issn_eissn = Column(String(50))
+    journal_access_type = Column(String(50))
+    journal_subject = Column(String(50))
+    grant_participation = Column(String(50))
+    funder_information = Column(String(150))
+    full_coverage_reason = Column(String(150))
+    original_apc_usd = Column(String(50))
+    contractual_apc_usd = Column(String(50))
+    library_apc_portion_usd = Column(String(50))
+    author_apc_portion_usd = Column(String(50))
+    payment_note = Column(String(150))
+    cdl_notes = Column(String(150))
+    license_chosen = Column(String(50))
+    journal_bucket = Column(String(50))
+    agreement_manager_profile_name = Column(String(50))
+    publisher_status = Column(String(50))
 
 class Database:
     def __init__(self, db_connect_str):
         self.engine = create_engine(db_connect_str)
 
     def findall(self, sql, params=None):
-        with self.engine.connect() as connection:
-            results = connection.execute(sql, params or ())
+        with self.engine.connect() as conn:
+            results = conn.execute(sql, params or ())
             results_dict = [dict(row) for row in results.fetchall()]
             return results_dict
+
+    def insert(self, db_table, values):
+        """insert multiple rows to a db table
+        """ 
+        with self.engine.connect() as conn:
+            print(values)
+            #result = conn.execute(db_table.insert().values(values))
+            result = conn.execute(insert(db_table, values))
+
 
     def close(self):
         self.engine.dispose()
@@ -33,7 +86,8 @@ def find_records(db_connect_str, select_query, params=None):
             records = db.findall(text(select_query), params)
             db.close()
             return records
-        except:
+        except SQLAlchemyError as e:
+            print("DB Select error: {}".format(e))
             return None
     return None
 
@@ -43,3 +97,51 @@ def find_tact_transactions(db_connect_str, id):
     return find_records(db_connect_str, SELECT_TACT, params)
 
 
+def insert_tact_transactions(db_connect_str, table_name, values):
+    """
+    Args:
+        db_connect_str: database connection string
+        table_name: table name
+        values: list of dictionary
+    """
+    if values:
+        try:
+            db = Database(db_connect_str)
+            db.insert(get_transactions_table(), values)
+            db.close
+        except SQLAlchemyError as e:
+            print("DB insert error: {}".format(e))
+
+def get_transactions_table():
+    return table("transactions",
+      column("publisher"),
+      column("doi"),
+      column("article_title"),
+      column("corresponding_author"),
+      column("corresponding_author_email"),
+      column("uc_institution"),
+      column("institution_identifier"),
+      column("document_type"),
+      column("eligible"),
+      column("inclusion_date"),
+      column("uc_approval_date"),
+      column("article_access_type"),
+      column("article_license"),
+      column("journal_name"),
+      column("issn_eissn"),
+      column("journal_access_type"),
+      column("journal_subject"),
+      column("grant_participation"),
+      column("funder_information"),
+      column("full_coverage_reason"),
+      column("original_apc_usd"),
+      column("contractual_apc_usd"),
+      column("library_apc_portion_usd"),
+      column("author_apc_portion_usd"),
+      column("payment_note"),
+      column("cdl_notes"),
+      column("license_chosen"),
+      column("journal_bucket"),
+      column("agreement_manager_profile_name"),
+      column("publisher_status")
+            )
