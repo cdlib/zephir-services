4540d8a02849499477d80e49dbaa6644dd78a314
Jing Jiang <jing.jiang@ucop.edu>
2022-07-06 17:57:38 -0700

    logging info based on CID minting results

--- prepare_zephir_records/cid_minter/cid_minter.py
+++ prepare_zephir_records/cid_minter/cid_minter.py
@@ -51,16 +51,41 @@ class CidMinter:
             logging.info(f"Local minter: Found matched CID: {matched_cids} by OCNs: {ocns}")
         else:
             logging.error(f"Local minter error: Found more than one matched CID: {matched_cids} by OCNs: {ocns}")
-            logging.info(results)
 
         if assigned_cid:
+            if assigned_cid != current_cid:
+                logging.info(f"htid {htid} changed CID from: {current_cid} to: {assigned_cid}")
             return assigned_cid
 
         logging.info(f"Find CID in Zephir Database by OCNs: {ocns}")
         results = cid_inquiry_by_ocns(ocns, self._zephir_db, self._leveldb_primary_path, self._leveldb_cluster_path) 
         logging.info(f"Minting results by OCNs: {results}")
 
-        assigned_cid = results['min_cid']
+        if results:
+            num_of_matched_oclc_clusters = results.get('num_of_matched_oclc_clusters')
+            num_of_matched_zephir_clusters = results.get('num_of_matched_zephir_clusters')
+            assigned_cid = results.get('min_cid')
+            cid_list = list(dict.fromkeys(results.get('cid_ocn_clusters')));
+
+            if num_of_matched_oclc_clusters > 1:
+                logging.info("ZED code: pr0096 - Record OCNs match more than one OCLC Concordance clusters")
+
+            if num_of_matched_zephir_clusters == 0:
+                logging.info(f"Zephir minter: No CID found by OCNs: {ocns}")
+            else:
+                logging.info("Zephir minter: Found matched CID: {matched_cids} by OCNs: {ocns}")
+
+            if num_of_matched_zephir_clusters > 1:
+                msg_detail = f"Record with OCLCs ({ocns}) matches {num_of_matched_zephir_clusters} CIDs ({cid_list}) used {assigned_cid}"
+                if len(ocns) > 1:
+                    logging.warn(f"ZED code: pr0090 - Record with OCLC numbers match more than one CID. {msg_detail}")
+                else:
+                    logging.warn(f"ZED code: pr0091 - Record with one OCLC matches more than one CID. {msg_detail}")
+
+            if assigned_cid:
+                if assigned_cid != current_cid:
+                    logging.info(f"htid {htid} changed CID from: {current_cid} to: {assigned_cid}")
+                return assigned_cid
 
         logging.info(f"Find CID in Zephir Database by contribsys IDs: {sysid}")
         results = self._zephir_db.find_zephir_clusters_by_contribsys_ids([sysid])
