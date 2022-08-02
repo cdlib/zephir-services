166470226e13ebf21e28535fa7e8be793ce195d3
Jing Jiang <jing.jiang@ucop.edu>
2022-08-01 18:05:53 -0700

    combined creating table and insert data for creating local minter

--- prepare_zephir_records/cid_minter/tests/test_local_cid_minter/prepare_cid_minter_datasets.sql
+++ prepare_zephir_records/cid_minter/tests/test_local_cid_minter/prepare_cid_minter_datasets.sql
@@ -1,3 +1,14 @@
+DROP TABLE IF EXISTS cid_minting_store;
+
+CREATE TABLE IF NOT EXISTS cid_minting_store (
+        id integer PRIMARY KEY AUTOINCREMENT,
+        type text DEFAULT NULL,
+        identifier text DEFAULT NULL,
+        cid text DEFAULT NULL,
+	updated DATETIME DEFAULT CURRENT_TIMESTAMP,
+        UNIQUE (type,identifier)
+);
+
 INSERT INTO cid_minting_store(type, identifier, cid)
 VALUES
 ("ocn", "8727632", "002492721"),
