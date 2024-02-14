a783752e10f64cb1d4881c547d0528dded310ed3
Raiden van Bronkhorst <rvanbronkhorst@gmail.com>
2024-02-14 15:06:47 -0800

    Implement get_value_from_record, start comparison code
    Added new tests

--- scripts/marc_comparison/match_up_file.py
+++ scripts/marc_comparison/match_up_file.py
@@ -1,27 +1,85 @@
 import argparse
 import sys
 
-from pymarc import parse_xml_to_array
+from pymarc import parse_xml_to_array, Field
+from collections import Counter
 
-def marc_compare(file1, file2, id1, id2):
+
+def get_value_from_location(record, loc): # 974$u   record.get("974")    .get("u")
+    split = loc.split("$")
+
+    current_value = record
+
+    while len(split):
+        if current_value is None:
+            return None
+        current_value = current_value.get(split.pop(0))
+
+    if isinstance(current_value, Field):
+        return current_value.value()
+
+    return current_value
+
+
+def compare_record(record1, record2, idloc1, idloc2):
+
+    differences = Counter()
+
+    def compare_field(location):
+        r1_value = get_value_from_location(record1, location)
+        r2_value = get_value_from_location(record2, location)
+
+        if r1_value != r2_value:
+            differences[location] = 1
+    
+    id1 = get_value_from_location(record1, idloc1)
+    id2 = get_value_from_location(record2, idloc2)
+
+    if id1 != id2:
+        raise ValueError(f"IDs do not match: {id1} != {id2}")
+
+    for field in ["260$a", "260$b", "260$c", "264$a", "264$b", "264$c"]:
+        compare_field(field)
+
+        
+    if differences.total() > 0:
+        print("HTID:", id1)
+        
+    for k, v in differences.items():
+        print(f"First file: {k} = {get_value_from_location(record1, k)}")
+        print(f"Second file: {k} = {get_value_from_location(record2, k)}")
+        
+    return differences
+
+
+def marc_compare(file1, file2, idloc1, idloc2):
     print(file1)
     print(file2)
     
-    len1 = load_and_count_records(file1)
-    len2 = load_and_count_records(file2)
+    records1 = load_records(file1)
+    records2 = load_records(file2)
 
+    len1 = len(records1)
+    len2 = len(records2)
     if len1 != len2:
         raise ValueError(f"Number of records in {file1} ({len1}) does not match the number of records in {file2} ({len2}).")
+
+    differences = Counter()
+    
+    for i in range(len(records1)):
+        new_diffs = compare_record(records1[i], records2[i], idloc1, idloc2)
+
+        differences.update(new_diffs)
     
     print(f"Number of records compared: {len1}")
     return 0
 
-def load_and_count_records(file_path):
+def load_records(file_path):
     # Load MARC records from an XML file
     with open(file_path, "r") as f:
         records = parse_xml_to_array(f)
 
-    return len(records)
+    return records
     # print(f"{len(records)=}")
     # # print(type(records[0].get("008")))
     # # print(records[0].get("008"))
@@ -34,14 +92,14 @@ def main():
     parser = argparse.ArgumentParser(description="Compare two MARCXML files.")
     parser.add_argument("--file1", help="First MARC XML file to compare", required=True)
     parser.add_argument("--file2", help="Second MARC XML file to compare", required=True)
-    parser.add_argument("--id1", help="ID 1", required=True)
-    parser.add_argument("--id2", help="ID 2", required=True)
+    parser.add_argument("--idloc1", help="ID 1", required=True)
+    parser.add_argument("--idloc2", help="ID 2", required=True)
     # Parse arguments
     args = parser.parse_args() 
 
     try:
         # run command with argument
-        return_code = marc_compare(args.file1, args.file2, args.id1, args.id2)
+        return_code = marc_compare(args.file1, args.file2, args.idloc1, args.idloc2)
     except Exception as err:
         print(err, file=sys.stderr)
         sys.exit(1) 
--- scripts/marc_comparison/tests/test_match_up_file.py
+++ scripts/marc_comparison/tests/test_match_up_file.py
@@ -2,7 +2,9 @@ import os
 import sys
 # import re
 import pytest
-from match_up_file import main 
+from match_up_file import main
+
+from match_up_file import get_value_from_location, parse_xml_to_array
 
 def test_record_number_match(td_tmpdir, capsys, pytestconfig):
     # Prepare the argument list for the CLI
@@ -10,7 +12,7 @@ def test_record_number_match(td_tmpdir, capsys, pytestconfig):
     second_file = os.path.join(td_tmpdir, "second.xml")
     first_id = "HOL$p"
     second_id = "974$u"
-    sys.argv = ["match_up_id.py", "--file1", first_file, "--file2", second_file, "--id1", first_id, "--id2", second_id]
+    sys.argv = ["match_up_id.py", "--file1", first_file, "--file2", second_file, "--idloc1", first_id, "--idloc2", second_id]
 
     # Execute the CLI command, expecting it to handle the argument correctly
     with pytest.raises(SystemExit) as pytest_e:
@@ -36,7 +38,7 @@ def test_record_number_mismatch(td_tmpdir, capsys, pytestconfig):
     second_file = os.path.join(td_tmpdir, "third.xml")
     first_id = "HOL$p"
     second_id = "974$u"
-    sys.argv = ["match_up_id.py", "--file1", first_file, "--file2", second_file, "--id1", first_id, "--id2", second_id]
+    sys.argv = ["match_up_id.py", "--file1", first_file, "--file2", second_file, "--idloc1", first_id, "--idloc2", second_id]
 
     # Execute the CLI command, expecting it to handle the argument correctly
     with pytest.raises(SystemExit) as pytest_e:
@@ -62,7 +64,7 @@ def test_file_not_found(td_tmpdir, capsys, pytestconfig):
     second_file = os.path.join(td_tmpdir, "does_not_exist.xml")
     first_id = "HOL$p"
     second_id = "974$u"
-    sys.argv = ["match_up_id.py", "--file1", first_file, "--file2", second_file, "--id1", first_id, "--id2", second_id]
+    sys.argv = ["match_up_id.py", "--file1", first_file, "--file2", second_file, "--idloc1", first_id, "--idloc2", second_id]
 
     # Execute the CLI command, expecting it to handle the argument correctly
     with pytest.raises(SystemExit) as pytest_e:
@@ -82,3 +84,37 @@ def test_file_not_found(td_tmpdir, capsys, pytestconfig):
     assert pytest_e.type == SystemExit
     assert pytest_e.value.code == 1
 
+
+def test_get_record_location(td_tmpdir):
+    records = parse_xml_to_array(os.path.join(td_tmpdir, "first.xml"))
+    
+    assert get_value_from_location(records[0], "245$a") == "Slovenské vyst̕ahovalectvo /"
+    assert get_value_from_location(records[0], "001") == "mdp.39015018415946"
+    assert get_value_from_location(records[0], "110") == None
+    assert get_value_from_location(records[0], "110$a") == None
+
+
+def test_record_ids_mismatch(td_tmpdir, capsys, pytestconfig):
+    first_file = os.path.join(td_tmpdir, "first.xml")
+    second_file = os.path.join(td_tmpdir, "second.xml")
+    first_id = "245$a"
+    second_id = "974$u"
+    sys.argv = ["match_up_id.py", "--file1", first_file, "--file2", second_file, "--idloc1", first_id, "--idloc2", second_id]
+
+    # Execute the CLI command, expecting it to handle the argument correctly
+    with pytest.raises(SystemExit) as pytest_e:
+        main()
+
+    # Capture the output
+    out, err = capsys.readouterr()
+
+    # Output the captured output to the respective streams for debugging if necessary
+    print(out, file=sys.stdout)
+    print(err, file=sys.stderr)
+
+    # Assert the expected output and exit code
+    assert f"{first_file}" in out, ""
+    assert f"{second_file}" in out, ""
+    assert "IDs do not match" in err, ""
+    assert pytest_e.type == SystemExit
+    assert pytest_e.value.code == 1
