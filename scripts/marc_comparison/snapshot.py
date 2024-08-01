4907afdf37e4822e718f0ca9c9b90013de30b6bb
Raiden van Bronkhorst <rvanbronkhorst@gmail.com>
2024-08-01 15:27:08 -0700

    Update snapshot, create HTID-CID explode script, update packages

--- scripts/marc_comparison/snapshot.py
+++ scripts/marc_comparison/snapshot.py
@@ -8,39 +8,41 @@ from collections import OrderedDict
 
 import yaml
 import pandas as pd
-from pymarc import XMLWriter, parse_xml_to_array
+from pymarc import parse_json_to_array
 from sqlalchemy import create_engine, text
 from sqlalchemy.engine.url import URL
 from sqlalchemy.orm import sessionmaker
 
 # Define a dictionary to map 'type' to 'drivername'
 DATABASE_DRIVER_MAP = {
-    'sqlite': 'sqlite',
-    'mysql': 'mysql+mysqlconnector',  # Example for MySQL using mysqlconnector
+    "sqlite": "sqlite",
+    "mysql": "mysql+mysqlconnector",  # Example for MySQL using mysqlconnector
 }
 
 DEFAULT_DB_CONFIG_FILE = "db.yml"
 DEFAULT_DB_ENV = "production"
 
+
 def prompt_for_mysql_details():
     print("No database configuration found. Please enter MySQL connection details.")
     db_config = {
-        'type': 'mysql',
-        'host': input("Enter MySQL host: "),
-        'user': input("Enter MySQL username: "),
-        'password': getpass.getpass("Enter MySQL password: "),
-        'port': input("Enter MySQL port (default 3306): ") or 3306,
-        'database': input("Enter MySQL database name: "),
+        "type": "mysql",
+        "host": input("Enter MySQL host: "),
+        "user": input("Enter MySQL username: "),
+        "password": getpass.getpass("Enter MySQL password: "),
+        "port": input("Enter MySQL port (default 3306): ") or 3306,
+        "database": input("Enter MySQL database name: "),
     }
     return db_config
+
 
 def initialize_database_session(db_config_path, db_env=DEFAULT_DB_ENV):
     db_config = None
 
     # If no config, look for default config. If not, prompt for MySQL details
     if not db_config_path and not os.path.exists(DEFAULT_DB_CONFIG_FILE):
-            db_config = prompt_for_mysql_details()
-            return db_config
+        db_config = prompt_for_mysql_details()
+        return db_config
     else:
         db_config_path = db_config_path or DEFAULT_DB_CONFIG_FILE
 
@@ -56,15 +58,15 @@ def initialize_database_session(db_config_path, db_env=DEFAULT_DB_ENV):
             print(f"Error parsing YAML file: {e}", file=sys.stderr)
             sys.exit(1)
         except KeyError:
-                print(
+            print(
                 f"Error: The environment '{db_env}' was not found in the database configuration file.",
                 file=sys.stderr,
             )
-                sys.exit(1)
+            sys.exit(1)
 
     # Retrieve the database configuration for the specified environment
     # Use the 'type' field from db_config to get the 'drivername'
-    db_type = db_config.get('type')
+    db_type = db_config.get("type")
     drivername = DATABASE_DRIVER_MAP.get(db_type)
     if not drivername:
         print(f"Error: Unsupported database type '{db_type}'.", file=sys.stderr)
@@ -79,13 +81,13 @@ def initialize_database_session(db_config_path, db_env=DEFAULT_DB_ENV):
         url = str(URL.create(drivername, database=database))
     elif db_config["type"] in "mysql":
         url = URL.create(
-                drivername,
-                username=db_config.get("user"),
-                password=db_config.get("password"),
-                host=db_config.get("host"),
-                port=db_config.get("port"),
-                database=db_config.get("database"),
-            )
+            drivername,
+            username=db_config.get("user"),
+            password=db_config.get("password"),
+            host=db_config.get("host"),
+            port=db_config.get("port"),
+            database=db_config.get("database"),
+        )
     else:
         print(
             f"Error: The drivername '{db_config['drivername']}' is not supported.",
@@ -116,38 +118,69 @@ def query_record_information_from_db(session, ids):
 
     # SQL databases have limits on the number of items in the IN clause, so chunk the IDs
     chunk_size = 500  # Adjust based on your database's performance and limits
-    chunks = [ids[i:i + chunk_size] for i in range(0, len(ids), chunk_size)]
+    chunks = [ids[i : i + chunk_size] for i in range(0, len(ids), chunk_size)]
 
     print("Querying records...", file=sys.stderr)
 
     for chunk in chunks:
         # Generate the placeholders for each chunk
-        placeholders = ','.join([':id{}'.format(i) for i in range(len(chunk))])
-        
+        placeholders = ",".join([":id{}".format(i) for i in range(len(chunk))])
+
         # Construct the query with dynamic placeholders
         query_str = f"""
-        SELECT zr.id as htid, zr.cid, zr.namespace, zr.source_record_number, attr.name as attr_name, attr.dscr as attr_description, reasons.name as reason_name, reasons.dscr as reason_description, zr.identifiers_json
+        SELECT zr.id as htid, 
+        zr.cid, 
+        zr.namespace,
+        zr.source_record_number, 
+        attr.name as attr_name, 
+        attr.dscr as attr_description, 
+        reasons.name as reason_name, 
+        reasons.dscr as reason_description, 
+        zr.identifiers_json,
+        zf.metadata_json
         FROM zephir_records as zr LEFT JOIN rights_current as rc ON zr.id = rc.volume_identifier
         LEFT JOIN attributes as attr ON rc.attr = attr.id
         LEFT JOIN reasons ON rc.reason = reasons.id
+        LEFT JOIN zephir_filedata as zf ON zf.id = zr.id
         WHERE zr.id IN ({placeholders})
         """
 
         # Prepare parameters as a dictionary {placeholder_name: value}
-        params = {f'id{i}': chunk[i] for i in range(len(chunk))}
+        params = {f"id{i}": chunk[i] for i in range(len(chunk))}
 
         result = session.execute(text(query_str), params).fetchall()
 
         found_ids = set(row[0] for row in result)  # Collect found IDs for comparison
         if result:
             for row in result:
-                id, cid, namespace, source_record_number, attr_name, attr_dscr, reason_name, reason_dscr, identifiers_json = row
+                (
+                    id,
+                    cid,
+                    namespace,
+                    source_record_number,
+                    attr_name,
+                    attr_dscr,
+                    reason_name,
+                    reason_dscr,
+                    identifiers_json,
+                    metadata_json
+                ) = row
                 oclcs = get_oclcs_from_json(identifiers_json)
+
+                # Get information from record's 008
+                record = parse_json_to_array(metadata_json)[0]
+                record_008 = record.get("008")
+                record_008_6_15 = record_008.data[6:15] if record_008 else None
+                record_008_28 = record_008.data[28] if record_008 else None
+                record_008_17 = record_008.data[17] if record_008 else None
 
                 ordered_dict = OrderedDict()
                 ordered_dict["id"] = id
                 ordered_dict["cid"] = cid
                 ordered_dict["oclcs"] = ",".join(oclcs)
+                ordered_dict["008/6-14"] = record_008_6_15
+                ordered_dict["008/28"] = record_008_28
+                ordered_dict["008/17"] = record_008_17
                 ordered_dict["namespace"] = namespace
                 ordered_dict["source_record_number"] = source_record_number
                 ordered_dict["attr_name"] = attr_name
@@ -157,17 +190,18 @@ def query_record_information_from_db(session, ids):
                 ordered_dict["identifiers_json"] = identifiers_json
 
                 zephir_records.append(ordered_dict)
-        
+
         # Update missing_ids by removing found_ids
         missing_ids = [id for id in missing_ids if id not in found_ids]
-    
+
     if missing_ids:
         print(
             f"Warning: IDs not found in the database: {', '.join(missing_ids)}",
             file=sys.stderr,
         )
-        
+
     return zephir_records
+
 
 def get_oclcs_from_json(json_str):
     try:
@@ -180,6 +214,7 @@ def get_oclcs_from_json(json_str):
     except json.JSONDecodeError:
         return []
 
+
 def write_to_csv(records, output_file):
     try:
         print("Writing to file...", file=sys.stderr)
@@ -188,8 +223,11 @@ def write_to_csv(records, output_file):
         print(f"Error: The file {output_file} was not found.", file=sys.stderr)
         sys.exit(1)
     except Exception as e:
-        print(f"An unexpected error occurred while writing to file: {e}", file=sys.stderr)
+        print(
+            f"An unexpected error occurred while writing to file: {e}", file=sys.stderr
+        )
         sys.exit(1)
+
 
 def process_and_generate_snapshot(input_file, output_file, db_config_path, db_env):
     # Load IDs from a file
@@ -210,7 +248,7 @@ def process_and_generate_snapshot(input_file, output_file, db_config_path, db_en
     write_to_csv(zephir_records, output_file)
 
     # print success message with how many records were exported
-    print(f"Finished. {len(zephir_records)} records exported",  file=sys.stderr)
+    print(f"Finished. {len(zephir_records)} records exported", file=sys.stderr)
 
     return 0
 
@@ -289,7 +327,6 @@ def main():
         print(f"An unexpected error occurred: {e}", file=sys.stderr)
         sys.exit(1)
 
+
 if __name__ == "__main__":
     main()
-
-
